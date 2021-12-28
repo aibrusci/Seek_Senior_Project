29e9c6df91e093386b6230b47d587787c3ae53ef
JillianQuinn <jillian@jillianq.com>
2021-12-27 17:19:41 -0800

    card

--- App.tsx
+++ App.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Searchbar from './Components/SearchBar/Searchbar';
 import SearchBar  from 'react-native-elements';
-import Logo from './Components/Logo/Logo'
+import Logo from './Components/Logo/Logo';
+import ActivityCard from './Components/ActivityCard/ActivityCard';
 
 
 export default function App() {
@@ -12,6 +13,9 @@ export default function App() {
       <Logo/>
       <Searchbar pageType="home"/>
       <StatusBar style="auto" />
+      <View style={styles.card}> 
+        <ActivityCard  title="COYA Taco Night" date="Tuesday Nights 5-9pm" image="https://s3-media0.fl.yelpcdn.com/bphoto/kkR5Sb3WeGAAVRLC6dAIOQ/o.jpg" savedIcon={false}></ActivityCard>
+      </View>
     </View>
   );
 }
@@ -23,4 +27,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  card: {
+    margin: 25
+  }
 });
