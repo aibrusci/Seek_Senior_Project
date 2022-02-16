cb175fd8a80f24157f25dc97c3c917be95406113
Tyra Krivonak <tkrivona@calpoly.edu>
2022-02-16 13:43:20 -0800

    set searchpage as main

--- navigation/tabs.tsx
+++ navigation/tabs.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Image, Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MapPage from '../Pages/MapPage';
+import SearchPage from '../Pages/SearchPage';
 import SavePage from '../Pages/SavePage';
 import UserPage from '../Pages/UserPage';
 
@@ -24,7 +25,7 @@ export default function tabs (){
             }
         }}
         >
-            <Tab.Screen name="MapPage" component={MapPage} options={{
+            <Tab.Screen name="SearchPage" component={SearchPage} options={{
                 tabBarIcon: ({focused}) => (
                     <View style={{alignItems: 'center', justifyContent: 'center'}}>
                         <Image 
